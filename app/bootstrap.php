e3ff6268cc74e7ced29d46966030f5fa0a47bf2d
Dmytro Savin <savin.dmitry@gmail.com>
2015-07-06 11:38:14 +0200

    Fix error pages

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -125,10 +125,10 @@ $app->error(function (\Exception $e, Request $request, $code) use ($app) {
 
     // 404.html, or 40x.html, or 4xx.html, or error.html
     $templates = array(
-        'Metamoris/views/errors/' . $code . '.html.twig',
-        'Metamoris/views/errors/' . substr($code, 0, 2) . 'x.html.twig',
-        'Metamoris/views/errors/' . substr($code, 0, 1) . 'xx.html.twig',
-        'Metamoris/views/errors/default.html.twig',
+        'errors/' . $code . '.html.twig',
+        'errors/' . substr($code, 0, 2) . 'x.html.twig',
+        'errors/' . substr($code, 0, 1) . 'xx.html.twig',
+        'errors/default.html.twig',
     );
 
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)),
--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 // everything relative to the root directory
 chdir(  dirname( dirname(__FILE__))  );
-define('APPLICATION_ENV', 'dev');
+define('APPLICATION_ENV', 'prod');
 ini_set('display_errors', 0);
 
 $app = require 'app/bootstrap.php';
